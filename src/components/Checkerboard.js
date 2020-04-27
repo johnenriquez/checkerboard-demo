ee321c29789b44023081a25f8cc5abeed0b5dae1
John Enriquez <john.enriquez@gmail.com>
2020-04-27 15:30:12 -0700

    adds dynamic player rows to state

--- src/components/Checkerboard.js
+++ src/components/Checkerboard.js
@@ -1,13 +1,45 @@
 import React from 'react';
 
 class Checkerboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.places = this.setUpPieces();
+  }
+
+  setUpPieces() {
+    // this is a meta-board
+    let places = [];
+    let boardSize = this.props.size;
+
+    // player 2 is "1" while player 1 is "0"
+    let player2row = new Array(boardSize).fill(1);
+    let player1row = new Array(boardSize).fill(0);
+    let emptyRow = new Array(boardSize).fill(null);
+
+    // now fill the rows
+    for (let row = 0; row < boardSize; row++) {
+      // player 2 is the top two rows
+      // player 1 is the bottom two rows
+      if (row === 0 || row === 1) {
+        places.push(player2row);
+      } else if (row === boardSize-2 || row === boardSize-1) {
+        places.push(player1row);
+      } else {
+        places.push(emptyRow);
+      }
+    }
+    
+    return places;
+  }
 
   renderRow(rowIndex, boardSize) {
     let row = [];
 
     // add `boardSize` number of squares to the row 
-    for (let i = 0; i < boardSize; i++) {
-      row.push(<div className="square" key={`${rowIndex}-${i}`}></div>);
+    for (let j = 0; j < boardSize; j++) {
+      let piece = this.board[rowIndex][j];
+      let square = <div className="square" key={`${rowIndex}-${j}`}>{piece}</div>;
+      row.push(square);
     }
 
     // designate whether row is even or odd for CSS
