5242338b85738cbbea9b8b6aaa00c49c9017f2f6
John Enriquez <john.enriquez@gmail.com>
2020-04-27 14:00:26 -0700

    renders hard-coded 8x8 checkerboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,99 @@
 import React from 'react';
 import './styles.css';
 
+class Checkerboard extends React.Component {
+  render() {
+    return (
+      <div id="checkerboard">
+        <div class="row">
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+        </div>
+        <div class="row">
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+        </div>
+        <div class="row">
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+        </div>
+        <div class="row">
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+        </div>
+        <div class="row">
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+        </div>
+        <div class="row">
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+        </div>
+        <div class="row">
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+        </div>
+        <div class="row">
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+          <div class="white_square"></div>
+          <div class="black_square"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      checkerboard
+      <Checkerboard size="8" />
     </div>
   );
 }
