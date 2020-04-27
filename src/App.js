7eb4cafa861300ca18d3025bba9c9616fa87090f
John Enriquez <john.enriquez@gmail.com>
2020-04-27 14:22:43 -0700

    makes board dynamic

--- src/App.js
+++ src/App.js
@@ -1,101 +1,52 @@
 import React from 'react';
 import './styles.css';
 
+
 class Checkerboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 8
+    }
+  }
+
+  renderRow(rowIndex, boardSize) {
+    let row = [];
+    for (let i = 0; i < boardSize; i++) {
+      row.push(<div className="square"></div>);
+    }
+    if (rowIndex % 2 === 0) {
+      return (
+        <div class="row row-even">{row}</div>
+      );
+    } else {
+      return (
+        <div className="row row-odd">{row}</div>
+      );
+    }
+  }
+
   render() {
+    let boardSize = this.state.size;
+    let checkerboard = [];
+    for (let i = 0; i < boardSize; i++) {
+      checkerboard.push(this.renderRow(i, boardSize));
+    }
     return (
-      <div id="checkerboard">
-        <div class="row">
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-        </div>
-        <div class="row">
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-        </div>
-        <div class="row">
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-        </div>
-        <div class="row">
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-        </div>
-        <div class="row">
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-        </div>
-        <div class="row">
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-        </div>
-        <div class="row">
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-        </div>
-        <div class="row">
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-          <div class="white_square"></div>
-          <div class="black_square"></div>
-        </div>
-      </div>
+      <div id="checkerboard">{checkerboard}</div>
     );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Checkerboard size="8" />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Checkerboard />
+      </div>
+    );
+  }
 }
 
 export default App;
